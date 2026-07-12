0ac35444ef80a283e38df995fa53b63e0d36eb21
Radu Catalina <raducatalinalexandra@gmail.com>
2026-07-12 18:08:13 +0300

    refactor: simplify game logic and reduce code duplication

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -11,12 +15,14 @@ document.querySelector('.check').addEventListener('click', function () {
   // When there is no input
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number!';
+    //document.querySelector('.message').textContent = 'No number!';
+    displayMessage('No number!');
   }
 
   // When player wins
   else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct number!';
+    // document.querySelector('.message').textContent = 'Correct number!';
+    displayMessage('Correct number!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.check').classList.add('win');
     if (score > highscore) {
@@ -29,6 +35,23 @@ document.querySelector('.check').addEventListener('click', function () {
       origin: { y: 0.6 },
     });
   }
+
+  // When guess is wrong
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      //   guess > secretNumber ? 'Too high!' : 'Too low!';
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      //document.querySelector('.message').textContent = 'You lost the game!';
+      displayMessage('You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+/*
 
   // When guess is too high
   else if (guess > secretNumber) {
@@ -53,12 +76,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
-});
+
+
+*/
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  //document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
